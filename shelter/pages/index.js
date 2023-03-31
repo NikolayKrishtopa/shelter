bff255debed77eca732580c9ab070170528d47f7
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-04-01 03:22:37 +0400

    feat: scss and tablet header

--- shelter/pages/index.js
+++ shelter/pages/index.js
@@ -1,4 +1,4 @@
-import './index.css';
+import './index.scss';
 import { PAGES } from '../utils/constants';
 import { HELPS } from '../utils/constants';
 import { PETS } from '../utils/constants';
--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,19 @@ module.exports = (env) => {
             'postcss-loader',
           ],
         },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            mode === 'development'
+              ? 'style-loader'
+              : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
       ],
     },
     plugins: [
