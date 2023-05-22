70221e5ba13fbd479d713187df56f9d1e9aaa9f1
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-05-22 21:15:36 +0400

    fix: add folder into dist

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
       main: './shelter/pages/index.js',
     },
     output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname,'shelter', 'dist'),
       filename: 'main.js',
       publicPath: '',
     },
